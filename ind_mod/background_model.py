9e0077af67ab01880ba9b0997881d4bac926810e
Robert James <robert.james.19@ucl.ac.uk>
2024-01-23 18:24:58 +1100

    Sample from full background model.

--- ind_mod/background_model.py
+++ ind_mod/background_model.py
@@ -54,16 +54,17 @@ class BackgroundModel():
             raise RuntimeError(f'Could not find half life for component: {component_name}')
         time_constant_ns = (half_life_years/ np.log(2)) * 1e9 * 3600. * 24. * 365.25
 
-        weights = []
-        for cycle, times in self.annual_cycles.items():
-            cycle_integral = np.exp(-times[0].value / time_constant_ns) - np.exp(-times[1].value / time_constant_ns)
-            weights.append(cycle_integral * self.exposures[cycle])
-        weights = weights / np.sum(weights)
-
         t_start_global = self.annual_cycles[list(self.annual_cycles)[0]][0].value
         t_end_global = self.annual_cycles[list(self.annual_cycles)[-1]][1].value
         t_total = t_end_global - t_start_global
         decay_factor = time_constant_ns / t_total * (1. - np.exp(-t_total / time_constant_ns))
+
+        weights = []
+        for cycle, times in self.annual_cycles.items():
+            cycle_integral = np.exp(-(times[0].value - t_start_global) / time_constant_ns) - \
+                np.exp(-(times[1].value - t_start_global) / time_constant_ns)
+            weights.append(cycle_integral * self.exposures[cycle])
+        weights = weights / np.sum(weights)
 
         try:
             df_sample = self.background_model[component_name].sample(decay_factor=decay_factor)
@@ -83,4 +84,17 @@ class BackgroundModel():
         df_sample['time'] = times_sample
 
         return df_sample
+
+    def sample_model(self):
+        df_sample_full = pd.DataFrame()
+        df_sample_full_list = []
+
+        for background_component in self.background_model.keys():
+            df_sample_component = self.sample_component(background_component)
+            df_sample_component['source'] = background_component
+            df_sample_full_list.append(df_sample_component)
+
+        df_sample_full = pd.concat(df_sample_full_list, ignore_index=True)
+
+        return df_sample_full
         
