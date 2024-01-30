Correct normalisation for rate hist; also return residuals and errors. NOTE: need to add in error on mean value of each annual cycle.

ind_mod/analysis.py
@@ -9,53 +9,71 @@ import multihist as mh
 
 export, __all__ = im.exporter()
 
-class RateHistHelper():
+class RateResidualsHelper():
     def __init__(self, data=None, 
                  annual_cycles=None, exposures=None,
-                 bins_per_cycle=20):
+                 bins_per_cycle=10,
+                 energy_min=2., energy_max=6.):
         self.data = data
         self.annual_cycles = annual_cycles
         self.exposures = exposures
         self.bins_per_cycle = bins_per_cycle
+        self.energy_min = energy_min
+        self.energy_max = energy_max
 
-    def get_hist(self):
-        all_residuals_bins = []
-        all_residuals_counts = []
+    def get_rate_residuals(self):
+        all_time_bins = []
+        all_rates = []
+        all_residuals = []
+        all_errors = []
+        
         first_annual_cycle = True
 
         for annual_cycle in self.annual_cycles.keys():
             data_this_cycle = self.data[self.data['annual_cycle'] == annual_cycle]
 
             rate_this_cycle = mh.Hist1d(data_this_cycle['time'], bins=self.bins_per_cycle)
-            rate_normalisation = self.exposures[annual_cycle]
-            rate_this_cycle.histogram = rate_this_cycle.histogram / rate_normalisation
 
-            all_residuals_counts.append(rate_this_cycle.histogram)
+            rate_normalisation = (self.exposures[annual_cycle] / self.bins_per_cycle) * \
+                (self.energy_max - self.energy_min)
+            
+            all_rates.append(rate_this_cycle.histogram / rate_normalisation)
+
+            average_rate_this_cycle = np.mean(all_rates[-1])
+            all_residuals.append(all_rates[-1] - average_rate_this_cycle)
+
+            all_errors.append(np.sqrt(rate_this_cycle.histogram)  / rate_normalisation)
+
             if first_annual_cycle:
-                all_residuals_bins.append(rate_this_cycle.bin_edges)
+                all_time_bins.append(rate_this_cycle.bin_edges)
                 first_annual_cycle = False
             else:
-                all_residuals_bins.append(rate_this_cycle.bin_edges[1:])
+                all_time_bins.append(rate_this_cycle.bin_edges[1:])
 
-        all_residuals_bins = np.concatenate(all_residuals_bins)
-        all_residuals_counts = np.concatenate(all_residuals_counts)
+        all_time_bins = np.concatenate(all_time_bins)
+        all_rates = np.concatenate(all_rates)
+        all_residuals = np.concatenate(all_residuals)
+        all_errors = np.concatenate(all_errors)
 
-        hist = mh.Hist1d.from_histogram(all_residuals_counts, bin_edges=all_residuals_bins)
+        rate_hist = mh.Hist1d.from_histogram(all_rates, bin_edges=all_time_bins)
 
-        return hist
+        return rate_hist, all_residuals, all_errors
 
 @export
 class Analysis():
-    def __init__(self, spectrum_file_folder=None, model_file=None):
+    def __init__(self, spectrum_file_folder=None, model_file=None,):
         self.bg_model = im.BackgroundModel(spectrum_file_folder=spectrum_file_folder, model_file=model_file)
 
-    def get_toy_rate_hist(self, bins_per_cycle=20):
+    def get_toy_rate_residuals(self, bins_per_cycle=20):
         toy_data = self.bg_model.sample_model()
         annual_cycles = self.bg_model.annual_cycles
         exposures = self.bg_model.exposures
 
-        hist_helper = RateHistHelper(data=toy_data, 
-                                     annual_cycles=annual_cycles, exposures=exposures)
-        hist = hist_helper.get_hist()
+        hist_helper = RateResidualsHelper(data=toy_data,
+                                          annual_cycles=annual_cycles,
+                                          exposures=exposures,
+                                          energy_min=self.bg_model.energy_min,
+                                          energy_max=self.bg_model.energy_max)
+        rate_hist, residuals, errors = hist_helper.get_rate_residuals()
 
-        return hist
+        return rate_hist, residuals, errors